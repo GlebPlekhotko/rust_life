570b98e9177c76562e5d0656b18d4ee256de67af
GlebPlekhotko <orionhere@gmail.com>
2023-09-17 22:22:57 +0300

    Fixed the random populate density test
    The actual density never equals to the given value. The original tolerance was
    5% up or down, but it occurred to be too tight and test has been failing time
    to time. So I made it 10%.

--- tests/field_tests/populate_randomly.rs
+++ tests/field_tests/populate_randomly.rs
@@ -21,11 +21,14 @@ fn more_than_one_density() {
 fn zero_point_five_density() {
     let mut new_field = Field::create(10, 20, Cliff);
     
-    let expected_alive_top = 10 * 20 / 2 + 10 * 20 / 20;
-    let expected_alive_bot = 10 * 20 / 2 - 10 * 20/ 20;
+    /* Obviously the actual density won't be equal to 0.5 So test passes if 
+       deviation is no more than 10% up or down. */
+    let expected_alive_top = 10 * 20 / 2 + 10 * 20 / 100 * 10;
+    let expected_alive_bot = 10 * 20 / 2 - 10 * 20 / 100 * 10;
 
     new_field.populate_randomly(0.5);
     
-    assert_eq!(true, new_field.alive < expected_alive_top);
-    assert_eq!(true, new_field.alive > expected_alive_bot);
+
+    assert_eq!(true, new_field.alive <= expected_alive_top);
+    assert_eq!(true, new_field.alive >= expected_alive_bot);
 }
